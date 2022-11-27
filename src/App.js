8aded5086fd3cb8e68af0aee5b109d84acfe5d9f
mdashik7723 <mdashik7723@gmail.com>
2022-11-28 01:34:59 +0600

    --

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [color, setColor] = useState("");
+    return (
+        <div align="center">
+            <div style={{backgroundColor: color, height: "100vh"}} className=" w-12">
+
+                <input type="text"
+                       value={color}
+                       placeholder="Input Color Name"
+                       onChange={(e) => setColor(e.target.value)}/>
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
